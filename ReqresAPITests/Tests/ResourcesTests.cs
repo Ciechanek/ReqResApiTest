6259a6017c8e0a99dd3896ab8fc7fd534bf3278a
Mateusz Ciechan <Mateusz_Ciechan@edwards.com>
2022-10-27 12:46:59 +0200

    List Of Resources Test

---
+++ ReqresAPITests/Tests/ResourcesTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using Newtonsoft.Json;
+using ReqresAPITests.Requests;
+using ReqresAPITests.Responses;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ReqresAPITests.Tests
+{
+    public class ResourcesTests : BaseTest
+    {
+        [Fact]
+        public void GetListOfResources()
+        {
+            ResourcesRequests request = new ResourcesRequests(Client);
+            RestResponse response = request.GetListOfResource();
+            Resources jsonResponse = JsonConvert.DeserializeObject<Resources>(response.Content);
+            Assert.Equal("cerulean", jsonResponse.Data[0].Name);
+        }
+    }
+}
